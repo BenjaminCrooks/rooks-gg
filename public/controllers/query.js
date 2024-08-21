fe07afdb3ad4ae76721c21f62087ce1dc57a97ac
Benjamin Crooks <54723255+BenjaminCrooks@users.noreply.github.com>
2024-08-21 02:41:55 -0400

    Add files via upload

--- public/controllers/query.js
+++ public/controllers/query.js
@@ -2,10 +2,15 @@ let matchesModel = require("../models/matchSchema.js")
 
 
 function aggregationMatch (req, res, next) {
+	let queryMatch = [ {gameVersion: new RegExp("^14.1[0-9]")} ]
+
+	if (req.baseUrl != "") { queryMatch.push({gameEndedInEarlySurrender: false}) }
+	// if (req.query.patch === undefined) { queryMatch.push({gameVersion: new RegExp("^14.1[0-9]")}) }
+	
 	if (res.locals.aggregation == undefined) {
 		res.status(400).redirect("../400")
 	} else {
-		res.locals.aggregation.unshift({$match: {gameEndedInEarlySurrender: false}})
+		res.locals.aggregation.unshift({$match: {$and: queryMatch}})
   next()
 	}
 }
--- public/controllers/selection.js
+++ public/controllers/selection.js
@@ -1,7 +1,10 @@
 exports.selectionNavigation = function(req, res) {
 	res.render("selection-navigation", {
-		pageTitle: res.locals.pageTitle,
-		urlPath: res.locals.path,
-		champions: res.locals.champions
+		// pageTitle
+		
+		// path
+		// links
+		// search
+		// champions
 	})
 }
